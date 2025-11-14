16dea410dff8d7d8576f43c8eb0d34057c83e9d9
Corvidale <sasha.corvidale@gmail.com>
2025-11-14 09:18:47 +0100

    Added vector class

--- CalculatingAngleAndVector/Program.cs
+++ CalculatingAngleAndVector/Program.cs
@@ -9,8 +9,8 @@ namespace CalculatingAngleAndVector
 
     internal class Program
     {
+        private Vector startPoint = new Vector(4,2);
 
-        private string name = "test";
         static void Main(string[] args)
         {
             Program program = new Program();
@@ -18,8 +18,27 @@ namespace CalculatingAngleAndVector
         }
         private void start()
         {
-            Console.WriteLine(name);
+            Console.WriteLine(startPoint.getX());
             Console.ReadLine();
         }
+
+        private class Vector
+        {
+            float x, y;
+            public Vector(float x, float y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public float getX()
+            {
+                return x;
+            }
+            public float getY()
+            {
+                return y;
+            }
+        }
     }
+    
 }
