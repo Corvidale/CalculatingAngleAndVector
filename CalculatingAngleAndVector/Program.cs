2e1f19988f9e6d7dbeed64cc98e70c34d1d6febf
Corvidale <sasha.corvidale@gmail.com>
2025-11-14 09:03:08 +0100

    testing if i remember how to c#

--- CalculatingAngleAndVector/Program.cs
+++ CalculatingAngleAndVector/Program.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace CalculatingAngleAndVector
 {
+
     internal class Program
     {
+
+        private string name = "test";
         static void Main(string[] args)
         {
-
+            Program program = new Program();
+            program.start();
+        }
+        private void start()
+        {
+            Console.WriteLine(name);
+            Console.ReadLine();
         }
     }
 }
